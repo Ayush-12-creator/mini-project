50f003fb0264751889bd1f02a8dcca8089931158
Ayush Keshan <ayushraunak@gmail.com>
2023-04-14 22:43:43 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number: "))
+b=a=int(input("Enter second number: "))
+print("The value of",a,"+",3,"is:",a+b)
+print("The value of",a,"-",3,"is:",a-b)
+print("The value of",a,"/",3,"is:",a/b)
+print("The value of",a,"//",3,"is:",a//b)
+print("The value of",a,"**",3,"is:",a**b)
+print("The value of",a,"%",3,"is:",a%b)
